94058d7b99914da5633b3a41a954b4ce601c7f0b
Edward Banner <edward@Edwards-MacBook-Air.local>
2025-12-08 21:09:05 -0500

    Read in doomgeneric from disk

--- boot_sector.asm
+++ boot_sector.asm
@@ -8,10 +8,18 @@ extern main
 _start:
     ; Set up stack pointer
     mov esp, 0x7c00
+
+    ; Read in one sector of doomgeneric from disk
+    mov ah, 0x02   ; BIOS function for reading from disk
+    mov al, 1      ; Read 1 sector (512 byte chunk)
+    mov ch, 0      ; Cylinder number (which 3D ring)
+    mov cl, 2      ; Sector number (which slice of the pie [ring])
+    mov dh, 0      ; Head number (which level of the 3D ring [depth])
+    mov bx, 0x7e00 ; Buffer to store read data
+    int 0x13       ; BIOS interrupt for disk operations
     
     ; --- Enter protected mode ---
     cli
-
     lgdt [gdt_ptr]
     mov eax, cr0
     or  eax, 1              ; PE=1
@@ -41,8 +49,6 @@ protected_mode_entry:
 
     ; Re-enable interrupts now that IDT is set up
     sti
-
-    mov eax, 0xDEADBEEF
 
     call main
 
