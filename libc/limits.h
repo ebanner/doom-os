138de32199aa9a624d5e848dbc0ab6a3070c02e7
Edward Banner <edward@Edwards-MacBook-Air.local>
2025-12-10 20:53:09 -0500

    Add image for two's complement values

--- libc/limits.h
+++ libc/limits.h
@@ -5,9 +5,16 @@
  *
    (insert-image
     (create-image
-     (expand-file-name "twos-complement.png" default-directory)
+     (expand-file-name "img/twos-complement.png" default-directory)
+      nil nil :scale 0.4
+    )
+   ) 
+
+   (insert-image
+    (create-image
+     (expand-file-name "img/range-of-values.png" default-directory)
       nil nil :scale 0.25
     )
-   )
+   )  
  *
  */
