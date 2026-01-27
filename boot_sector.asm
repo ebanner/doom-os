c4c3c8edb0ccf06a2797afa9cace871bf3989a08
Edward Banner <edward@Edwards-MacBook-Air.local>
2026-01-26 20:35:44 -0500

    Read in 4 boot sectors
    os.img is currently like 1.3K so the last 300 bytes or so weren't
    getting read in, causing garbage to be executed

--- boot_sector.asm
+++ boot_sector.asm
@@ -11,13 +11,13 @@ _start:
 
     ; Read in one sector of doomgeneric from disk
     mov ah, 0x02   ; BIOS function for reading from disk
-    mov al, 1      ; Read 1 sector (512 byte chunk)
+    mov al, 4      ; Read 4 sectors (512 byte chunks)
     mov ch, 0      ; Cylinder number (which 3D ring)
     mov cl, 2      ; Sector number (which slice of the pie [ring])
     mov dh, 0      ; Head number (which level of the 3D ring [depth])
     mov bx, 0x7e00 ; Buffer to store read data
     int 0x13       ; BIOS interrupt for disk operations
-    
+
     ; --- Enter protected mode ---
     cli
     lgdt [gdt_ptr]
